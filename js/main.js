017bd395b8f9fbd78185c6ee8775debe9df2a861
LeRo <stilov94@gmail.com>
2024-01-01 21:35:46 +0500

    up

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
     setInterval(changeBackground, 5000);
     function changeBackground() {
         const max = 3;
-        const number = Math.floor(Math.random() * max + 1);
+        const min = 1;
+        const number = Math.floor(Math.random() * (max - min + 1)) + min;
         block.style.backgroundImage = `url('./../img/header/${number}.jpg')`;
         console.log(number);
     }
